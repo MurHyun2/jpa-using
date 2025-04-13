eb203f6bf2534be54422d42eb8d6cd31c018eb8e
MurHyun2 <eddy152264@gmail.com>
2025-04-13 16:03:45 +0900

    섹션 8.상품 등록

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -12,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -27,7 +29,7 @@ public class MemberController {
     @PostMapping(value = "/members/new")
     public String create(@Valid MemberForm form, BindingResult result) {
 
-        if(result.hasErrors()) {
+        if (result.hasErrors()) {
             return "members/createMemberForm";
         }
 
@@ -41,6 +43,10 @@ public class MemberController {
         return "redirect:/";
     }
 
-
-
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
